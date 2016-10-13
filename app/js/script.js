db4229b9a81dddb39107c0f6d5001aaf966d26c3
jacorderox <jacorderox@localhost.localdomain>
2016-10-13 00:08:46 -0500

    A simple animation to show the update of the centroids in the clustering process was implemented

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,107 @@
 // function used to start kmeans simmulation
 
 var radius = 5;
-var width = 900;
+var width = 500;
 var height = 400;
+var colors = ["#641e16", "#512e5f", "#154360", "#0e6251", "#145a32", " #7d6608", "#784212", " #7b7d7d", "#4d5656", "#1b2631"];
+
+var iterations;
 
 function startKMeans(){
-  //console.log(document.getElementById("numberOfClusters"));
-  //console.log(document.getElementById("numberOfPoints"));
-  var clusters = parseInt(document.getElementById("numberOfClusters").value);
+
+  iterations = 10;
+  // validate number of cluster restriction
+  var numberOfClusters = parseInt(document.getElementById("numberOfClusters").value);
+  if (numberOfClusters > colors.length){
+    alert("Number of clusters must be lesser or equal to 10");
+    return;
+  }
+  //console.log(numberOfClusters);
+
   var numberOfPoints = parseInt(document.getElementById("numberOfPoints").value);
-  //console.log("starting KMeans algorithm");
+
+  // kmeans initialization
   var arrayOfPoints = generateRandomPoints(radius, width - radius, radius, height - radius, numberOfPoints);
-  plotPoints(arrayOfPoints);
+  var centroids = generateRandomCentroids(radius, width - radius, radius, height - radius, numberOfClusters);
+  //console.log(centroids);
+  plotPoints(arrayOfPoints.concat(centroids));
+
+  var interval = setInterval(function(){
+      if (iterations <= 0){
+        clearInterval(interval);
+      }
+      [assignments, arrayOfPoints] = assignCentroids(arrayOfPoints, centroids);
+      console.log("remaining iterations: " + iterations.toString());
+      centroids = updateCentroids(arrayOfPoints, assignments, numberOfClusters);
+      plotPoints(arrayOfPoints.concat(centroids));
+      iterations -= 1;
+    }, 5000);
+}
+
+
+
+function assignCentroids(points, centroids){
+    // iterate per point
+    var assignedCentroids = []
+    for (i = 0; i < points.length; i++){
+      // iterate per centroids
+      centroid_index = 0
+      centroid_distance = compute_distance(points[i], centroids[0]);
+      //console.log("centroid_distance");
+      //console.log(centroid_distance);
+      for (j = 1; j < centroids.length; j++){
+          var distance = compute_distance(points[i], centroids[j]);
+          //console.log("distance");
+          //console.log(distance);
+          if (distance < centroid_distance){
+            centroid_index = j
+            centroid_distance = distance;
+        }
+      }
+      points[i].color_value = centroids[centroid_index].color_value;
+      assignedCentroids.push(centroid_index);
+    }
+    return [assignedCentroids, points];
+}
+
+function compute_distance(point, centroid){
+  return Math.sqrt(Math.pow(point.x_axis - centroid.x_axis, 2) + Math.pow(point.y_axis - centroid.y_axis, 2))
+}
+
+/**
+Need to protect against division by zero and integer overflow
+**/
+
+function updateCentroids(points, assignments, numberOfCentroids){
+  //console.log("number of centroids");
+  //console.log(numberOfCentroids);
+  var newCentroids = generateEmptyCentroids(numberOfCentroids);
+  //console.log(newCentroids);
+  var pointsPerCentroid = arrayOfZeros(numberOfCentroids);
+  for (i = 0; i < points.length; i++){
+    var currentCentroid = newCentroids[assignments[i]];
+    var point = points[i];
+    //console.log(currentCentroid);
+    //console.log(point);
+    currentCentroid.x_axis += point.x_axis;
+    currentCentroid.y_axis += point.y_axis;
+    pointsPerCentroid[assignments[i]] += 1.0;
+  }
+
+  for (i = 0; i < newCentroids.length; i++){
+    newCentroids[i].x_axis /= pointsPerCentroid[i];
+    newCentroids[i].y_axis /= pointsPerCentroid[i];
+  }
+
+  return newCentroids;
+}
+
+function arrayOfZeros(size){
+  var zeros = [];
+  for (i = 0; i < size; i++){
+    zeros.push(0);
+  }
+  return zeros;
 }
 
 function getRandomInt(min, max){
@@ -21,11 +111,36 @@ function getRandomInt(min, max){
 function generateRandomPoints(min_x, max_x, min_y, max_y, size){
   var points = [];
   for (i = 0; i < size; i++){
-    var point = {'x_axis': getRandomInt(min_x, max_x), 'y_axis': getRandomInt(min_y, max_y)}
+    var point = {'x_axis': getRandomInt(min_x, max_x), 'y_axis': getRandomInt(min_y, max_y), 'radius': 4, 'color_value': 'black'}
     points.push(point);
   }
   return points;
 }
+
+function generateRandomCentroids(min_x, max_x, min_y, max_y, size){
+  var centroids = [];
+  for (i = 0; i < size; i++){
+    var centroid = {'x_axis': getRandomInt(min_x, max_x), 'y_axis': getRandomInt(min_y, max_y), 'radius':8,  'color_value': selectColor(i + 1, size)}
+    centroids.push(centroid);
+  }
+  return centroids;
+}
+
+function generateEmptyCentroids(size){
+  var centroids = [];
+  for (i = 0; i < size; i++){
+    var centroid = {'x_axis': 0.0, 'y_axis': 0.0, 'radius':8,  'color_value': selectColor(i + 1, size)}
+    centroids.push(centroid);
+  }
+  //console.log(centroids);
+  return centroids;
+}
+
+function selectColor(index, max_index){
+  //console.log(index % max_index);
+  return colors[index % max_index];
+}
+
 
 function plotPoints(points){
   // remove old svg element before plotting a new one
@@ -42,7 +157,6 @@ function plotPoints(points){
   var circleAttributes = circles
                          .attr("cx", function (d) { return d.x_axis; })
                          .attr("cy", function (d) { return d.y_axis; })
-                         .attr("r", radius)
-                         .style("fill", "blue");
-
+                         .attr("r", function (d) {return d.radius})
+                         .style("fill", function(d) { return d.color_value});
 }
